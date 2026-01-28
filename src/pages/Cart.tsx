dddabe297e409cd3f9c2cdbff10512b71fa98eb6
Mathismwat <mathism59139@gmail.com>
2026-01-28 11:55:08 +0100

    Création de la page Cart

---
+++ src/pages/Cart.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router";
+
+export default function Cart() {
+    return (
+        <div>
+            <h1>Shopping Cart</h1>
+            <Link to="/shop">Page Shop</Link>
+        </div>
+    );
+}
