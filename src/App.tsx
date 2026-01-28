0886f900a2c4d584a757307fd1c3f32b95b56c74
Mathismwat <mathism59139@gmail.com>
2026-01-28 14:29:50 +0100

    Activer le composant Cart dans la navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router";
 import Shop from './pages/Shop';
-// import Cart from './pages/Cart';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -9,9 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Shop />} />
         <Route path="/shop" element={<Shop />} />
-        {
-          //<Route path="/cart" element={<Cart />} />
-        }
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   )
