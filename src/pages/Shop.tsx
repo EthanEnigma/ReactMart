14f19cfbfaaf95c17783852fc50510d50c34783e
EthanEnigma <ethan.legris@outlook.com>
2026-02-02 20:34:00 +0100

    Feat/Style: Add a product sorting system + add footer / restyle the product cards and header

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,30 +1,90 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import Products from "../data/Products";
 import type { ProductProps } from "../data/Products";
 
 export default function Shop() {
-    const [product, setProduct] = useState<ProductProps>();
+    const [products, setProducts] = useState<ProductProps[]>([]);
+    const [activeCategory, setActiveCategory] = useState<string>("All");
+
+    useEffect(() => {
+        setProducts(Products)
+    }, [])
+
+    function Filter(category: string) {
+        setActiveCategory(category)
+
+        if (category === "All") {
+            setProducts(Products)
+        } else {
+            const filtered = Products.filter(
+                (product) => product.category === category
+            )
+            setProducts(filtered)
+        }
+    }
+
     return (
-        <div className="flex flex-col h-screen w-screen" id="document">
+        <div className="min-h-screen w-screen bg-gray-300 overflow-y-hidden" id="document">
+            <head>
+                <title>ReactMart • Shop</title>
+            </head>
 
             {/* HEADER */}
-            <div className="bg-[#7A8D9B] w-full p-4 flex justify-between items-center" id="header">
-                <h1 className="text-white font-bold text-xl">Shop</h1>
-                <nav className="flex gap-4">
-                    <Link className="text-white hover:underline" to="/">Home</Link>
-                    <Link className="text-white hover:underline" to="/Cart">Cart</Link>
+            <header className="z-1 relative p-4 flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 drop-shadow-lg/30">
+                <div className="absolute inset-0 bg-[url(/src/data/images/LeafBackground.jpg)] bg-center grayscale"></div>
+                <div className="absolute inset-0 bg-[#DABEB6]/70"></div>
+                <h1 className="z-2 text-white font-bold text-xl text-center mx-5 sm:text-left text-shadow-lg">ReactMart - Shop</h1>
+                <nav className="z-2 flex justify-center sm:justify-end gap-4 mx-5">
+                    <Link to="/" className="bg-[#EED0C6] px-3 py-2 rounded-md hover:scale-110 duration-100"><p className="text-white">Shop</p></Link>
+                    <Link to="/Cart" className="bg-[#EED0C6] px-3 py-2 rounded-md hover:scale-110 duration-100"><p className="text-white">Cart</p></Link>
                 </nav>
-            </div>
+            </header>
 
             {/* CONTENT */}
-            <div className="flex-1 overflow-auto p-4 w-full" id="body">
-                <div className="grid grid-cols-2 gap-4" id="productsContainer">
-                    { }
-                    <div className="bg-white text-black p-4 shadown rounded-ml" id="product">
-                        <h1>Product</h1>
-                    </div>
+            <div id="body" className="flex flex-col overflow-auto p-4">
+                <h2 className="text-3xl text-black text-center mt-5">Catégories :</h2>
+                <div className="text-center mb-5">
+                    <button onClick={() => Filter("All")} className={`px-5 py-2 m-1 rounded-md duration-150 ${activeCategory === "All" ? "!bg-[#EED0C6] !border-solid !border-2 !border-[#DABEB6]" : "!bg-[#7A8D9B]"}`}>Tout</button>
+                    <button onClick={() => Filter("Décoration")} className={`px-5 py-2 m-1 rounded-md duration-150 ${activeCategory === "Décoration" ? "!bg-[#EED0C6] !border-solid !border-2 !border-[#DABEB6]" : "!bg-[#7A8D9B]"}`}>Décoration</button>
+                    <button onClick={() => Filter("Maison")} className={`px-5 py-2 m-1 rounded-md duration-150 ${activeCategory === "Maison" ? "!bg-[#EED0C6] !border-solid !border-2 !border-[#DABEB6]" : "!bg-[#7A8D9B]"}`}>Maison</button>
+                    <button onClick={() => Filter("Cuisine")} className={`px-5 py-2 m-1 rounded-md duration-150 ${activeCategory === "Cuisine" ? "!bg-[#EED0C6] !border-solid !border-2 !border-[#DABEB6]" : "!bg-[#7A8D9B]"}`}>Cuisine</button>
+                    <button onClick={() => Filter("Art")} className={`px-5 py-2 m-1 rounded-md duration-150 ${activeCategory === "Art" ? "!bg-[#EED0C6] !border-solid !border-2 !border-[#DABEB6]" : "!bg-[#7A8D9B]"}`}>Art</button>
+                    <button onClick={() => Filter("Papeterie")} className={`px-5 py-2 m-1 rounded-md duration-150 ${activeCategory === "Papeterie" ? "!bg-[#EED0C6] !border-solid !border-2 !border-[#DABEB6]" : "!bg-[#7A8D9B]"}`}>Papeterie</button>
+                    <button onClick={() => Filter("Mode")} className={`px-5 py-2 m-1 rounded-md duration-150 ${activeCategory === "Mode" ? "!bg-[#EED0C6] !border-solid !border-2 !border-[#DABEB6]" : "!bg-[#7A8D9B]"}`}>Mode</button>
+                    <button onClick={() => Filter("Accessoires")} className={`px-5 py-2 m-1 rounded-md duration-150 ${activeCategory === "Accessoires" ? "!bg-[#EED0C6] !border-solid !border-2 !border-[#DABEB6]" : "!bg-[#7A8D9B]"}`}>Accessoires</button>
+                    <button onClick={() => Filter("Bijoux")} className={`px-5 py-2 m-1 rounded-md duration-150 ${activeCategory === "Bijoux" ? "!bg-[#EED0C6] !border-solid !border-2 !border-[#DABEB6]" : "!bg-[#7A8D9B]"}`}>Bijoux</button>
                 </div>
+                <div id="productsContainer" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-7xl mx-auto">
+                    {products.map((product) => (
+                        <div key={product.id} className="bg-white text-black p-4 my-3 drop-shadow-md/25 rounded-md overflow-hidden hover:scale-102 duration-150">
+                            <div className="relative flex justify-center h-50 w-full bg-gradient-to-b from-gray-100 to-[#EED0C6] rounded-md">
+                                <p className="absolute left-0 top-0 text-white opacity-90 bg-[#cccccc] px-3 py-1 m-2 rounded-md">{product.category}</p>
+                                <p className="absolute right-0 bottom-0 text-white bg-[#DABEB6] px-3 py-1 m-2 rounded-md">{product.price}€</p>
+                                <img src={product.images} className="max-w-full" />
+                            </div>
+                            <hr className="border-solid border-black border-1 rounded-md my-3" />
+                            <div className="h-25 w-full">
+                                <h2 className="text-xl text-center mb-2">{product.name}</h2>
+                                <p className="text-sm text-[#B2B9BF] mb-2">{product.description}</p>
+                            </div>
+                            <div className="" id="">
+
+                            </div>
+                            <a className="bg-[#7A8D9B] px-5 py-2 rounded-full hover:bg-green-300 hover:cursor-pointer duration-150"><span className="text-white">Ajouter au panier</span></a>
+                            <p id="quantity" className="text-sm my-3"></p>
+                        </div>
+                    ))}
+                </div>
+            </div >
+
+            {/* FOOTER */}
+            <div className="relative flex w-full h-50 items-center justify-center">
+                <div className="absolute inset-0 bg-[url(/src/data/images/LeafBackground.jpg)] bg-center grayscale"></div>
+                <div className="absolute inset-0 bg-[#EED0C6]/70"></div>
+                <h2 className="relative text-4xl text-center text-white">© ReactMart • Tout droit réservé</h2>
             </div>
-        </div>
+
+        </div >
     )
 }
