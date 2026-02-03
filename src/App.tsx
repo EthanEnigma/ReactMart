e0d21a18c40157128e3425bacaf73898ff0dd621
EthanEnigma <ethan.legris@outlook.com>
2026-02-03 17:08:39 +0100

    Feat: Add 'favorites' page route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router";
 import Shop from './pages/Shop';
 import Cart from './pages/Cart';
+impor Favorites from './pages/Favorites'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/shop" element={<Shop />} />
+          <Route path="/favorites" element={<Favorites />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
