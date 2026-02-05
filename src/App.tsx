2046aa6689cfe7c0c6c0fc22b701f9b0c9e2ac32
EthanEnigma <ethan.legris@outlook.com>
2026-02-05 12:29:34 +0100

    hotfix: Add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router";
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
+import Favorites from './pages/Favorites';
+import ProductPage from './pages/ProductPage';
+
+function App() {
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Shop />} />
+                    <Route path="/shop" element={<Shop />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path='/product/:id' element={<ProductPage />} />
+                    <Route path="/favorites" element={<Favorites />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App
